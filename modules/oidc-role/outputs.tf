d74ae556638f11679e50e9e5a82f2f5aa6815c84
jamieastley <6836556+jamieastley@users.noreply.github.com>
2026-02-11 23:12:20 +1100

    Revert "create oidc-role module"
    This reverts commit c2754b7f1627a38d382bf48e8dd7d26d0566352d.

--- modules/oidc-role/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "role_arn" {
-  value       = module.oidc_policies.oidc_role_arn
-  description = "The ARN of the IAM role created for GitHub Actions OIDC authentication."
-}
--- modules/oidc-role/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "oidc_provider_arn" {
-  type        = string
-  description = "The ARN of the OIDC provider to associate with the IAM role."
-  sensitive   = true
-}
