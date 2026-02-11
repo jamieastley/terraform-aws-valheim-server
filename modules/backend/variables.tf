cfcc3316b16d3c7224672012c26cbe2601f210f9
jamieastley <6836556+jamieastley@users.noreply.github.com>
2026-02-11 23:13:59 +1100

    remove backend module

--- modules/backend/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "app_name" {
-  description = "The name of the application. This will be used as a prefix for all resources created by this module."
-  type        = string
-  nullable    = false
-  default     = "valheim"
-}
